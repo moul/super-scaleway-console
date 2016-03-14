4bafefe80f957a26aa2bae8effb446cd64d6c4dc
Manfred Touron <m@42.am>
2016-03-14 12:39:29 +0100

    Add scaleway.user.js

---
+++ scaleway.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name           Super Scaleway Console
+// @version        1
+// @author         Manfred Touron
+// @namespace      http://m.42.am/
+// @description    Scaleway console++
+// @include        https://cloud.scaleway.com/*
+// @updateURL      https://raw.githubusercontent.com/moul/super-scaleway-console/master/scaleway.user.js
+// @downloadURL    https://raw.githubusercontent.com/moul/super-scaleway-console/master/scaleway.user.js
+// ==/UserScript==
+
+console.log("Hello from super-scaleway-console !");
