c27a5f1760665bc8f7747c1a7e63eb4fed8dbcaf
Manfred Touron <m@42.am>
2016-03-14 12:49:46 +0100

    Reduce height of listings

--- scaleway.user.js
+++ scaleway.user.js
@@ -1,12 +1,20 @@
 // ==UserScript==
 // @name           Super Scaleway Console
-// @version        1
+// @version        2
 // @author         Manfred Touron
 // @namespace      http://m.42.am/
 // @description    Scaleway console++
 // @include        https://cloud.scaleway.com/*
+// @run-at         document-end
 // @updateURL      https://raw.githubusercontent.com/moul/super-scaleway-console/master/scaleway.user.js
 // @downloadURL    https://raw.githubusercontent.com/moul/super-scaleway-console/master/scaleway.user.js
+// @grant          GM_addStyle
 // ==/UserScript==
 
-console.log("Hello from super-scaleway-console !");
+function main() {
+  console.log("Hello from super-scaleway-console !");
+  GM_addStyle(".table tbody>tr:not(.ng-table-group) { height: auto !important; }");
+  console.log("Goodbye from super-scaleway-console !");
+}
+
+window.setTimeout(main, 1000);
